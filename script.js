f867366489b112b2d4d8b9671d7199e989bb3176
Mohammad Abu Arida <74688331+AbuArida72@users.noreply.github.com>
2024-07-11 22:05:35 +0300

    final draft
    project is complete and does not require any more development.

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 const startText = document.getElementById('startText')
 const paddle1 = document.getElementById('paddle1');
 const paddle2 = document.getElementById('paddle2');
+const ball = document.getElementById('ball');
+const player1ScoreElement = document.getElementById('player1Score');
+const player2ScoreElement = document.getElementById('player2Score');
+const lossSound = document.getElementById('lossSound');
+const wallSound = document.getElementById('wallSound');
+const paddleSound = document.getElementById('paddleSound');
 
 // Game Variables
 let gameRunning = false;
@@ -10,6 +16,12 @@ let paddle1Speed = 0;
 let paddle1Y = 150;
 let paddle2Speed = 0;
 let paddle2Y = 150;
+let ballX= 290;
+let ballY= 190;
+let ballSpeedX = 2;
+let ballSpeedY = 2;
+let player2Score = 0;
+let player1Score = 0;
 
 //Game Constants
 const paddleAcceleration = 1;
@@ -31,8 +43,9 @@ function startGame() {
 
 function gameLoop() {
     if(gameRunning) {
-        updatePaddle1()
-        updatePaddle2()
+        updatePaddle1();
+        updatePaddle2();
+        moveBall();
         setTimeout(gameLoop, 8);
     }
 }
@@ -55,7 +68,7 @@ function updatePaddle1() {
     else {
         if (paddle1Speed > 0) {
             paddle1Speed = Math.max(paddle1Speed - paddleAcceleration, 0)
-        } else if (paddle1Speed > 0) {
+        } else if (paddle1Speed < 0) {
             paddle1Speed = Math.min(paddle1Speed + paddleAcceleration, 0)
         }
     }
@@ -82,7 +95,7 @@ function updatePaddle2() {
     else {
         if (paddle2Speed > 0) {
             paddle2Speed = Math.max(paddle2Speed - paddleAcceleration, 0)
-        } else if (paddle2Speed > 0) {
+        } else if (paddle2Speed < 0) {
             paddle2Speed = Math.min(paddle2Speed + paddleAcceleration, 0)
         }
     }
@@ -98,4 +111,67 @@ function updatePaddle2() {
     }
 
     paddle2.style.top = paddle2Y + 'px';
+}
+
+function moveBall() {
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
+
+    if (ballY >= gameHeight - ball.clientHeight || ballY <= 0) {
+        ballSpeedY = -ballSpeedY;
+        playSound(wallSound);
+    }
+
+    if (ballX >= gameWidth - paddle2.clientWidth - ball.clientWidth && 
+        ballY >= paddle2Y && 
+        ballY <= paddle2Y + paddle2.clientHeight) {
+        ballSpeedX = -ballSpeedX;
+        playSound(paddleSound);
+    }
+
+    if (ballX <= paddle1.clientWidth && 
+        ballY >= paddle1Y && 
+        ballY <= paddle1Y + paddle1.clientHeight) {
+        ballSpeedX = -ballSpeedX;
+        playSound(paddleSound);
+    }
+
+    if (ballX <= 0) {
+        player2Score++;
+        playSound(lossSound);
+        updateScoreboard();
+        resetBall();
+        pauseGame();
+    } else if (ballX >= gameWidth - ball.clientWidth) {
+        player1Score++;
+        playSound(lossSound);
+        updateScoreboard();
+        resetBall();
+        pauseGame();
+    }
+
+    ball.style.left = ballX + 'px';
+    ball.style.top = ballY + 'px';
+}
+
+function updateScoreboard() {
+    player1ScoreElement.textContent = player1Score;
+    player2ScoreElement.textContent = player2Score;
+}
+
+function resetBall() {
+    ballX = gameWidth / 2 - ball.clientWidth / 2;
+    ballY = gameHeight / 2 - ball.clientHeight / 2;
+    ballSpeedX = Math.random() > 0.5 ? 2 : -2;
+    ballSpeedY = Math.random() > 0.5 ? 2 : -2;
+}
+
+function pauseGame() {
+    gameRunning = false;
+    document.addEventListener('keydown', startGame);
+}
+
+function playSound(sound) {
+    sound.currentTime = 0;
+    sound.play();
 }
